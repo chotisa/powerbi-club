22893609ee7760452f421d4a3142244ecb1c0a99
chotisa <74693785+chotisa@users.noreply.github.com>
2020-11-19 11:48:36 +0700

    Update line_notify_covid.php

--- line_notify_covid.php
+++ line_notify_covid.php
@@ -7,7 +7,7 @@ curl_setopt($cv, CURLOPT_RETURNTRANSFER, 1);
  $js_array=json_decode($output, true);
 
  $notifyURL = "https://notify-api.line.me/api/notify";
- $accToken = "xh1EjQw8A4E77Bu8Onc3xXtfxiYXhFSKofGz7LBDr62";
+ $accToken = "jsPPw5dKTjIOrMJUwdAQtjVSYzh81ELJsiGyhUDUN70";
 $headers = array(
  'Content-Type: application/x-www-form-urlencoded',
  'Authorization: Bearer '.$accToken
